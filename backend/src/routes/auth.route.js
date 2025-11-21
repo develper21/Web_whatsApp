0eff587c193b9bdb62e9744b9b0542012673b929
develper21 <githubnarvin@gmail.com>
2025-11-21 23:59:16 +0530

    add auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { checkAuth, logout, requestOtp, updateProfile, verifyOtp } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/request-otp", requestOtp);
+router.post("/verify-otp", verifyOtp);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
